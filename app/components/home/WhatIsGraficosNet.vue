12567a6563d318339be6c28a9dfc1f893e290240
paulmelero <paul.melero@backmarket.com>
2026-02-01 20:10:02 +0100

    fix: Improve mobile hci

--- app/components/home/WhatIsGraficosNet.vue
+++ app/components/home/WhatIsGraficosNet.vue
@@ -9,7 +9,7 @@
     <div v-if="section" class="container sm:pt-32 px-10">
       <div class="sm:grid grid-cols-3 place-items-center flex flex-col gap-10">
         <div class="media flex justify-center items-start">
-          <BrandTheLogo class="opacity-20 group-hover:opacity-100 transition-opacity duration-300" />
+          <BrandTheLogo class="sm:opacity-20 sm:group-hover:opacity-100 transition-opacity duration-300" />
         </div>
         <ContentRenderer v-if="section" :value="section" class="col-span-2 article-post font-ibm" />
       </div>
