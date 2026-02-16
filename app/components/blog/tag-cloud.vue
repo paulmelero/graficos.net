cdc583e0ebbdcfe3f2c3f9ef5730f709d542e072
paulmelero <paul.melero@backmarket.com>
2026-02-16 21:38:03 +0100

    fix: Show more tags feature

--- app/components/blog/tag-cloud.vue
+++ app/components/blog/tag-cloud.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="card">
     <h3 class="font-ibm dark:font-thin mb-4">{{ sectionTitle }}</h3>
-    <ul v-if="Object.keys(tags).length" class="list-reset flex gap-2 flex-wrap">
-      <li v-for="(tag, i) in sortedTags" :key="i">
-        <blog-tag-media :name="tag" :size="tags[tag] || 1" class="capitalize" />
-      </li>
-    </ul>
+    <div v-if="Object.keys(tags).length">
+      <ul class="list-reset flex gap-2 flex-wrap mb-4">
+        <li v-for="(tag, i) in visibleTags" :key="i">
+          <blog-tag-media :name="tag" :size="tags[tag] || 1" class="capitalize" />
+        </li>
+      </ul>
+
+      <div v-if="hasMoreTags" class="overflow-hidden transition-all duration-300 ease-in-out" :style="expandedStyle">
+        <ul class="list-reset flex gap-2 flex-wrap pb-4">
+          <li v-for="(tag, i) in hiddenTags" :key="i">
+            <blog-tag-media :name="tag" :size="tags[tag] || 1" class="capitalize" />
+          </li>
+        </ul>
+      </div>
+
+      <button
+        v-if="hasMoreTags"
+        class="text-sm font-ibm dark:font-thin text-gray-dark dark:text-gray-light hover:underline flex items-center gap-1"
+        @click="isExpanded = !isExpanded"
+      >
+        {{ isExpanded ? 'Show less' : `Show ${hiddenTags.length} more tags` }}
+        <span class="transition-transform duration-300" :class="{ 'rotate-180': isExpanded }">↓</span>
+      </button>
+    </div>
     <p v-else class="text-sm text-gray-500">No tags found</p>
     <p v-if="!shouldHide" class="text-sm font-ibm dark:font-thin">
       Go back to <nuxt-link to="/blog">the blog</nuxt-link> to see all posts.
@@ -27,6 +46,17 @@ const props = withDefaults(
 )
 
 const sortedTags = computed(() => {
-  return Object.keys(props.tags).sort((a, b) => props.tags[b] - props.tags[a])
+  const tags = props.tags || {}
+  return Object.keys(tags).sort((a, b) => (tags[b] || 0) - (tags[a] || 0))
 })
+
+const isExpanded = ref(false)
+const visibleTags = computed(() => sortedTags.value.slice(0, 10))
+const hiddenTags = computed(() => sortedTags.value.slice(10))
+const hasMoreTags = computed(() => hiddenTags.value.length > 0)
+
+const expandedStyle = computed(() => ({
+  maxHeight: isExpanded.value ? '1000px' : '0px',
+  opacity: isExpanded.value ? '1' : '0',
+}))
 </script>
