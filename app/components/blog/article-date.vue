6a65ecd14fa3bb93d884532fd6950357ff07e99d
paulmelero <paul.melero@backmarket.com>
2026-06-24 23:59:14 +0200

    Fix duplicate dates because of modified time

--- app/components/blog/article-date.vue
+++ app/components/blog/article-date.vue
@@ -1,15 +1,18 @@
 <template>
   <base-texts-the-time v-if="date" :date="date" class="font-ibm block sm:inline-block" />
-  <span class="block sm:inline-block font-ibm text-xs">
-    <span v-if="modifiedDate"
-      ><span class="hidden sm:inline-block">&nbsp;</span>(<span class="text-xs">Last modified:</span>&nbsp;</span
+  <span v-if="modifiedDate && !sameDate" class="block sm:inline-block font-ibm text-xs">
+    <span><span class="hidden sm:inline-block">&nbsp;</span>(<span class="text-xs">Last modified:</span>&nbsp;</span
     ><base-texts-the-time v-if="modifiedDate" :date="modifiedDate" /><span v-if="modifiedDate">)</span>
   </span>
 </template>
 
 <script lang="ts" setup>
-defineProps<{
+const props = defineProps<{
   date?: string
   modifiedDate?: string
 }>()
+
+const sameDate = computed(() => {
+  return props.date === props.modifiedDate || props.date?.split('T')[0] === props.modifiedDate?.split('T')[0]
+})
 </script>
